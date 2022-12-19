3506231ef47518fd76ceba9d305df171a678f28c
kosuke <kosuke@KosukenoMacBook-Air.local>
2022-12-20 00:17:10 +0900

    docker環境構築、top/index.html.erbとski_resorts/search.index.erbのcontainer-fluidをcontainerに変更

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2022_12_15_052713) do
 
-  create_table "active_storage_attachments", force: :cascade do |t|
+  create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2022_12_15_052713) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
+  create_table "active_storage_blobs", charset: "utf8mb3", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -34,13 +34,13 @@ ActiveRecord::Schema.define(version: 2022_12_15_052713) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", force: :cascade do |t|
+  create_table "active_storage_variant_records", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "reviews", force: :cascade do |t|
+  create_table "reviews", charset: "utf8mb3", force: :cascade do |t|
     t.integer "user_id"
     t.integer "ski_resort_id"
     t.string "title"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2022_12_15_052713) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "ski_resorts", force: :cascade do |t|
+  create_table "ski_resorts", charset: "utf8mb3", force: :cascade do |t|
     t.string "resort_name"
     t.string "address"
     t.text "hp_url"
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(version: 2022_12_15_052713) do
     t.time "end_time"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
